06cb62b6d6d427c1e76d8bf857280529bc6c610d
osama miro <o.miro650@gmail.com>
2017-12-19 21:47:36 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
-def my_select(collection)
- # your code here!
- i = 0
- evens = []
- while i < collection.length
-   block_given?
-   evens << yield((collection[i]) % 2 =0))
-   i += 1
+def my_select(array)
+count = 0
+result = []
 
- end
- evens
+while count < array.size
+  if yield(array[count]) == true
+  result.push(array[count])
+end
+  count+=1
+end
+result
 end
